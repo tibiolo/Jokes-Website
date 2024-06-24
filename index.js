f3bd91ebbb8c476d3e6eb9444798d793e11b0299
tibiolo <161530252+tibiolo@users.noreply.github.com>
2024-06-24 21:02:45 +0100

    reverted last commit

--- index.js
+++ index.js
@@ -21,59 +21,25 @@ app.use(express.static("public"));
 // Jokes API
 const API_URL = "https://v2.jokeapi.dev/joke";
 
-// Global Filter Variables
-let filters;
-
 // Homepage Route
 app.get("/", async (req, res) => {
-    try {
-        const result = await axios.get(API_URL + "/Any", { params: { type: "twopart" }});
-        res.render(__dirname + "/views/index.ejs", {
-            jokeTitle: result.data.setup,
-            joke: result.data.delivery
-        });
-    } catch (error) {
-        console.error(error.message);
-    }
-});
-
-app.get("/next", async (req, res) => {
-    if (filters && filters.length > 0) {
-        try {
-            const result = await axios.get(API_URL + `/${filters.category}`, { params: { type: "twopart", blacklistFlags: `${filters.blacklist.join()}`}});
+    const result = await axios.get(API_URL + "/Any", { params: { type: "twopart" }});
     res.render(__dirname + "/views/index.ejs", {
         jokeTitle: result.data.setup,
         joke: result.data.delivery
     });
-        } catch (error) {
-            console.error(error.message);
-        }
-    } else {
-        try {
-            const result = await axios.get(API_URL + "/Any", { params: { type: "twopart" }});
-            res.render(__dirname + "/views/index.ejs", {
-                jokeTitle: result.data.setup,
-                joke: result.data.delivery
-            });
-        } catch (error) {
-            console.error(error.message);
-        }
-    }
-});
+})
 
 // Filters Route
 app.post("/filters", async (req, res) => {
-    try {
-        filters = req.body;
-        const result = await axios.get(API_URL + `/${filters.category}`, { params: { type: "twopart", blacklistFlags: `${filters.blacklist.join()}`}});
-        res.render(__dirname + "/views/index.ejs", {
-            jokeTitle: result.data.setup,
-            joke: result.data.delivery
-        });
-    } catch (error) {
-        console.error(error.message);
-    }
-});
+    let filters = req.body;
+    console.log(`${filters.blacklist.join()}`)
+    const result = await axios.get(API_URL + `/${filters.category}`, { params: { type: "twopart", blacklistFlags: `${filters.blacklist.join()}`}});
+    res.render(__dirname + "/views/index.ejs", {
+        jokeTitle: result.data.setup,
+        joke: result.data.delivery
+    })
+})
 
 
 // Server Listening
